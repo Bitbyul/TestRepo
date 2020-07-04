c91f9aea626f6848d61fdfa577233a453f66e934
Sangddung <ghrj1112@naver.com>
2020-07-04 14:17:33 +0900

    Update

--- Server/Server/CarPetServer.cpp
+++ Server/Server/CarPetServer.cpp
@@ -1,12 +1,1 @@
-﻿#include "Server.h"
-
-using namespace std;
-
-int main()
-{
-	string str;
-	cout << "Enter name: ";
-	cin >> str;
-	cout << "Hello " << str << endl;
-	return 0;
-}
+﻿
---
+++ Server/Server/Start.cpp
@@ -0,0 +1,10 @@
+#include "Server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Started car-pet Server";
+
+	return 0;
+}
